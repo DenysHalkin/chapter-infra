42790eed86b0367b79e16927cdf2a2a9d8659249
Denys Halkin <denis.galkin.91@gmail.com>
2023-09-07 15:23:14 +0200

    Add cloudfront logging

---
+++ chapter-infra-tf-modules/frontend-web-app/logging-s3.tf
@@ -0,0 +1,64 @@
+data "aws_canonical_user_id" "current" {}
+
+data "aws_cloudfront_log_delivery_canonical_user_id" "cloudfront" {}
+
+module "logging_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = "${var.project}-${var.env_name}-cloudfront-logging-${var.region_alias}"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  grant = [
+    {
+      type       = "CanonicalUser"
+      permission = "FULL_CONTROL"
+      id         = data.aws_canonical_user_id.current.id
+    },
+    {
+      type       = "CanonicalUser"
+      permission = "FULL_CONTROL"
+      id         = data.aws_cloudfront_log_delivery_canonical_user_id.cloudfront.id # Ref. https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html
+    }
+  ]
+
+  owner = {
+    id = data.aws_canonical_user_id.current.id
+  }
+
+  attach_deny_insecure_transport_policy  = true
+  attach_require_latest_tls_policy       = true
+  attach_deny_unencrypted_object_uploads = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "logs_rotation"
+      enabled = true
+
+      filter = {
+        prefix = ""
+      }
+
+      expiration = {
+        days = 1
+      }
+    }
+  ]
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+
+  tags = local.common_tags
+}
--- chapter-infra-tf-modules/frontend-web-app/web-app-cloudfront.tf
+++ chapter-infra-tf-modules/frontend-web-app/web-app-cloudfront.tf
@@ -3,11 +3,12 @@ module "web_app_cloudfront" {
   version = "3.2.1"
 
   #aliases = ["chapter-web.com"]
-  comment         = "Chapter Cloudfront ${title(var.env_name)} environement"
-  price_class     = "PriceClass_100"
-  enabled         = true
-  http_version    = "http2and3"
-  is_ipv6_enabled = true
+  comment             = "Chapter Cloudfront ${title(var.env_name)} environement"
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100"
+  enabled             = true
+  http_version        = "http2and3"
+  is_ipv6_enabled     = true
 
   origin = {
     frontend_web_app_s3 = {
@@ -28,15 +29,18 @@ module "web_app_cloudfront" {
 
   default_cache_behavior = {
     target_origin_id       = "frontend_web_app_s3"
-    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" #todo
     viewer_protocol_policy = "redirect-to-https"
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
-    compress        = true
-    query_string    = true
+    allowed_methods      = ["GET", "HEAD", "OPTIONS"]
+    cached_methods       = ["GET", "HEAD"]
+    compress             = true
+    query_string         = true
+    use_forwarded_values = false
+  }
 
-
+  logging_config = {
+    bucket = module.logging_bucket.s3_bucket_bucket_domain_name
   }
 
   tags = local.common_tags
